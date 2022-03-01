bade441521d33c007b17ed004eb020eb15f39c24
KJ Hsu <48618827+hikper@users.noreply.github.com>
2022-03-01 11:53:50 +0800

    fix lab1-2 lab1-3 bug

--- lab1/lab1-3.py
+++ lab1/lab1-3.py
@@ -11,6 +11,8 @@ import math
 def scale(img, height, width, n):
     newimg = np.zeros((height*n, width*n, 3), np.uint8)
     for i in range(height-1):
+        if i%10 == 0:
+            print(i)
         for j in range(width-1):
             for x in range(n):
                 for y in range(n):
@@ -21,7 +23,12 @@ def scale(img, height, width, n):
                     Trange = y
                     Drange = n-y
                     Center = (topCenter*Drange+downCenter*Trange)/n
-                    newimg[n*i+x,n*j+y] = Center
+                    newimg[n*i+y,n*j+x] = np.round(Center)
+#                     print(f"\tx {x}, y {y}")
+#                     print(f"\tTop: left {img[i,j]}, right {img[i,j+1]}, center {topCenter}")
+#                     print(f"\tDow: left {img[i+1,j]}, right {img[i+1][j+1]}, center {downCenter}")
+#                     print(f"\tCenter {Center}, newimg {newimg[n*i+x, n*j+y]}")
+                    
     return newimg
             
 
@@ -30,16 +37,12 @@ image = cv2.imread("lisa.jpeg")
 height, width, channels = image.shape
 
 
-newimg = scale(image,height,width,2)
+newimg = scale(image,height,width,3)
 
 cv2.imwrite("output3.jpg", newimg)
 cv2.imshow("new img", newimg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-# In[ ]:
-
 
 
 
