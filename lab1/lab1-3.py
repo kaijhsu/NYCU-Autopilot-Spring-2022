03485ffe5585cdee2f66a250645ecdcc6ea5c703
Sakuya0229 <a1301145@gmail.com>
2022-03-03 16:46:49 +0800

    fix lab1-3 range

--- lab1/lab1-3.py
+++ lab1/lab1-3.py
@@ -10,16 +10,18 @@ import math
 
 def scale(img, height, width, n):
     newimg = np.zeros((height*n, width*n, 3), np.uint8)
-    for i in range(height-1):
+    for i in range(height):
         if i%10 == 0:
             print(i)
-        for j in range(width-1):
+        for j in range(width):
             for x in range(n):
                 for y in range(n):
                     Lrange = x
                     Rrange = n-x
-                    topCenter = (float(Rrange/n)*img[i,j]+float(Lrange/n)*img[i,j+1])
-                    downCenter = (float(Rrange/n)*img[i+1,j]+float(Lrange/n)*img[i+1,j+1])
+                    ti = min(i+1,height-1)
+                    tj = min(j+1,width-1)
+                    topCenter = (float(Rrange/n)*img[i,j]+float(Lrange/n)*img[i,tj])
+                    downCenter = (float(Rrange/n)*img[ti,j]+float(Lrange/n)*img[ti,tj])
                     Trange = y
                     Drange = n-y
                     Center = (topCenter*Drange+downCenter*Trange)/n
@@ -28,9 +30,9 @@ def scale(img, height, width, n):
 #                     print(f"\tTop: left {img[i,j]}, right {img[i,j+1]}, center {topCenter}")
 #                     print(f"\tDow: left {img[i+1,j]}, right {img[i+1][j+1]}, center {downCenter}")
 #                     print(f"\tCenter {Center}, newimg {newimg[n*i+x, n*j+y]}")
-                    
+
     return newimg
-            
+
 
 
 image = cv2.imread("lisa.jpeg")
@@ -43,6 +45,3 @@ cv2.imwrite("output3.jpg", newimg)
 cv2.imshow("new img", newimg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
