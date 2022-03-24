8717e4dbb67f58fb40ff76ba062db62d788037c7
hikper <hikper@gmail.com>
2022-03-24 19:43:11 +0800

    lab4-1 pass

--- lab4/lab4-1.py
+++ lab4/lab4-1.py
@@ -1,10 +1,34 @@
 import cv2
+import numpy as np
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(2)  # device
 
-while True:
+patternSize = (9, 6)
+objpoints = []
+imgpoints = []
+
+while(True):
     ret, frame = cap.read()
-    # ret, corner = 
+    if ret == False:
+        print("Cannot read video")
+        break
+    objp = np.zeros((9*6, 3), np.float32)  # create h*w (0,0,0)
+    objp[:, :2] = np.mgrid[0:6, 0:9].T.reshape(-1, 2)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(frame, patternSize, None)
+    if ret == True:
+        objpoints.append(objp)
+        cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1),
+                         (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1))
+        imgpoints.append(corners)
+        if len(imgpoints) == 5:
+            break
 
     cv2.imshow('frame', frame)
     cv2.waitKey(33)
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+    objpoints, imgpoints, gray.shape[::-1], None, None)
+f = cv2.FileStorage("Calibration.xml", cv2.FILE_STORAGE_WRITE)
+f.write("intrinsic", mtx)
+f.write("distortion", dist)
+f.release()
