6195019aff41e3fcdbef3f511aa8410aff7d2944
hikper <hikper@gmail.com>
2022-04-12 17:12:26 +0800

    lab7 pass id 1

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -1,11 +1,10 @@
-
-
 import cv2
 import numpy as np
 import time
 import math
 from djitellopy import Tello
 from pyimagesearch.pid import PID
+
 
 
 def keyboard(self, key):
@@ -72,6 +71,9 @@ def main():
     yaw_pid.initialize()
 
     maxSpeed = 40
+    flag_5 = 1
+    flag_3 = 1
+    flag_1 = 1
 
     while (True):
         #drone.send_rc_control(0, 0, 0, 0)
@@ -89,12 +91,30 @@ def main():
             markerCorners, 15, intrinsic, distortion)
         z_update = 0
         y_update = 0
-        flag_5 = 1
-        flag_3 = 1
         #yaw_update = 0
         yaw_update_deg = 0
         if markerIds is not None:
-            if 0 in markerIds:
+            if 1 in markerIds and flag_1 == 1:
+                idx_1 = markerIds.tolist().index([1])
+                if tvec[idx_1, 0, 2] < 100 :
+                    height = int(drone.get_height())
+                    delta_height = height - 30
+                    if delta_height > 20:
+                        drone.move_down(delta_height)
+                    drone.move_forward(200)
+                    drone.move_up(30)
+                    z_update = 0
+                    y_update = 0
+                    time.sleep(6)
+                    flag_1 = 0
+                else:
+                    z_update = tvec[idx_1, 0, 2] - 90
+                    z_update = z_pid.update(z_update, sleep=0)
+                    if z_update > maxSpeed:
+                        z_update = maxSpeed
+                    elif z_update < -maxSpeed:
+                        z_update = -maxSpeed
+            elif 0 in markerIds:
                 idx_0 =markerIds.tolist().index([0])
                 try:
                     frame = cv2.aruco.drawAxis(
@@ -114,7 +134,8 @@ def main():
                     elif z_update < -maxSpeed:
                         z_update = -maxSpeed
 
-                    y_update = -tvec[idx_0, 0, 1] - 6
+                    y_update = -tvec[idx_0, 0, 1] - 10
+    
                     y_update = y_pid.update(y_update, sleep=0)
                     if y_update > maxSpeed:
                         y_update = maxSpeed
@@ -140,17 +161,33 @@ def main():
                     print(tvec[idx_0])
             elif 5 in markerIds and flag_5 == 1:
                 idx_5 = markerIds.tolist().index([5])
-                drone.move_left(60)
-                drone.move_forward(150)
-                flag_5 = 0
-            elif 3 in markerIds:
+                if tvec[idx_5, 0, 2] < 110 :
+                    z_update = 0
+                    drone.move_left(60)
+                    drone.move_forward(150)
+                    flag_5 = 0
+                    time.sleep(5)
+                height = drone.get_height()
+                delta_height = int(height - 130)
+                if abs(delta_height) > 20 :
+                    if delta_height < 0:
+                        drone.move_up(-1*delta_height)
+                    else:
+                        drone.move_down(delta_height)
+                z_update = tvec[idx_5, 0, 2] - 100
+                z_update = z_pid.update(z_update, sleep=0)
+                if z_update > maxSpeed:
+                    z_update = maxSpeed
+                elif z_update < -maxSpeed:
+                    z_update = -maxSpeed
+            elif 3 in markerIds and flag_3 == 1 and flag_5 == 0:
                 idx_3 = markerIds.tolist().index([3])
-                if tvec[idx_3, 0, 2] < 110 and flag_3 == 1:
+                if tvec[idx_3, 0, 2] < 110 :
                     z_update = 0
                     drone.move_right(80)
                     drone.move_forward(120)
                     flag_3 = 0
-                    sleep(5)
+                    time.sleep(5)
                 else:
                     z_update = tvec[idx_3, 0, 2] - 100
                     z_update = z_pid.update(z_update, sleep=0)
@@ -158,6 +195,7 @@ def main():
                         z_update = maxSpeed
                     elif z_update < -maxSpeed:
                         z_update = -maxSpeed
+                    
         key = cv2.waitKey(1)
         if key == -1:
             drone.send_rc_control(0, int(z_update), int(
