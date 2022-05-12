0e88a0bfb9d2b587c38020fac6832babf31a6355
Sakuya0229 <a1301145@gmail.com>
2022-05-12 19:32:34 +0800

    edge detect

--- lab10/lab10_color.py
+++ lab10/lab10_color.py
@@ -15,8 +15,16 @@ while(True):
     upper_range = np.array([130, 255, 255])
     mask = cv2.inRange(hsv_frame, lower_range, upper_range)
     result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    frame = cv2.cvtColor(result,cv2.COLOR_HSV2RGB)
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    blur_gray = cv2.GaussianBlur(gray,(15, 15), 0)
+    edges_frame = cv2.Canny(blur_gray, 30, 70)
+
     # for i in range(h):
     #     for j in range(w):
     #         print()
-    cv2.imshow('Result', result)
-    cv2.waitKey(0)
+    cv2.imshow('Result', edges_frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
